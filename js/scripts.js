6c20e6ed44b1785c0813d4c337c327a7500d4b1c
TI Sistemas - Rafael Silva do Nascimento <rafaelnascimento@focustextil.loc.br>
2016-06-16 18:55:49 -0300

    Home Desktop - Ajustes Finais

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,32 @@
-$(document).ready(function() {
-  var owl = $("section.carrossel > div"); 
-  owl.owlCarousel({
-    singleItem:true,
-  }); 
+$(function(){ 
+	(function() {
+		var brainvest = {
+	        init: function() {
+	            this.menu();
+	            this.carrossel();
+	        },
+	        menu: function() {
+        		var _opnd;
+	        	$('main header nav a').click(function() {
+	        		$('main header nav.s_menu, main header nav.s_language').hide();
+	        		if( $(this).attr('data-menu')!=undefined && $(this).attr('data-menu')!=null && $(this).attr('data-menu')!=_opnd ){
+	        			$('main header nav.'+$(this).attr('data-menu')).show();
+	        			 _opnd = $(this).attr('data-menu');
+	        		}else{
+	        			if($(this).attr('data-menu')==_opnd){
+							_opnd = null;
+	        			}
+	        		}
+
+	        	});
+	        },
+	        carrossel: function() {
+			  var owl = $("section.carrossel > div"); 
+			  owl.owlCarousel({
+			    singleItem:true,
+			  }); 
+	        }
+    	}
+		brainvest.init();
+	})();
 });
